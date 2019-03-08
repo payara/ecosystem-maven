edeb56c3de406e6e41b301a78026c9bb1863b7c6
Rudy De Busscher <rudy.de.busscher@payara.fish>
2019-03-08 16:58:16 +0100

    MP-40: Define Payara Micro 5.191 as default version

--- payara-micro-maven-plugin/src/main/java/fish/payara/maven/plugins/micro/StartMojo.java
+++ payara-micro-maven-plugin/src/main/java/fish/payara/maven/plugins/micro/StartMojo.java
@@ -74,7 +74,7 @@ public class StartMojo extends BasePayaraMojo {
     @Parameter(property = "javaPath")
     private String javaPath;
 
-    @Parameter(property = "payaraVersion", defaultValue = "5.184")
+    @Parameter(property = "payaraVersion", defaultValue = "5.191")
     private String payaraVersion;
 
     @Parameter(property = "payaraMicroAbsolutePath")
