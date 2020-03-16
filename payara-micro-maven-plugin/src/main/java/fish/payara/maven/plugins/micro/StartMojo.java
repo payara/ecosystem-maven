78448aec9c322c1a87466682c96f5c455d6c811c
Gaurav Gupta <gaurav.gupta.jc@gmail.com>
2020-03-16 10:54:23 +0530

    ECOSYS-122 Upgrade dependencies in payara-micro-maven-plugin to latest version

--- payara-micro-maven-plugin/src/main/java/fish/payara/maven/plugins/micro/StartMojo.java
+++ payara-micro-maven-plugin/src/main/java/fish/payara/maven/plugins/micro/StartMojo.java
@@ -74,7 +74,7 @@ public class StartMojo extends BasePayaraMojo {
     @Parameter(property = "javaPath")
     private String javaPath;
 
-    @Parameter(property = "payaraVersion", defaultValue = "5.193")
+    @Parameter(property = "payaraVersion", defaultValue = "5.201")
     private String payaraVersion;
 
     @Parameter(property = "payaraMicroAbsolutePath")
